6791a14dbe1e12c4a5972207e9ac73873a05bc66
Ben Findley <ben.g.findley@gmail.com>
2020-08-03 09:30:47 -0700

    cargo fmt

--- src/core/map/tile.rs
+++ src/core/map/tile.rs
@@ -208,11 +208,11 @@ mod tile_tests {
 			.boxed()
 	}
 
-    fn non_empty_tiletype_strategy() -> BoxedStrategy<TileType<DummyIO>> {
-        rand_dummy_tiletype_strategy()
-            .prop_filter("empty tiletype", |t| t.name != "")
-            .boxed()
-    }
+	fn non_empty_tiletype_strategy() -> BoxedStrategy<TileType<DummyIO>> {
+		rand_dummy_tiletype_strategy()
+			.prop_filter("empty tiletype", |t| t.name != "")
+			.boxed()
+	}
 
 	proptest!(
 		#[test]
@@ -225,15 +225,15 @@ mod tile_tests {
 		}
 	);
 
-    proptest!(
-        #[test]
-        fn non_empty_tiletypes_are_valid(tt in non_empty_tiletype_strategy()) {
+	proptest!(
+		#[test]
+		fn non_empty_tiletypes_are_valid(tt in non_empty_tiletype_strategy()) {
 			let mut dummy_io = DummyIO::default();
 			let mut tts = TileTypes::new();
-            let name = tt.name.clone();     // I really don't want to do this, but add_tile consumes
+			let name = tt.name.clone();     // I really don't want to do this, but add_tile consumes
 			if let Err(TileTypesError::InvalidTileTypeData(s)) = tts.add_tile(&mut dummy_io, tt) {
 				prop_assert!(false, "TileType {} marked invalid because {}", name, s);
 			}
-        }
-    );
+		}
+	);
 }
